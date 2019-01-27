cc08e6986c49a5d7c0fef41058ac73813e3e47da
Daniil Leksin <danilaleksin@gmail.com>
2019-01-27 22:51:31 +0300

    Process URL input. + Test.

--- lib/campaigns_service.rb
+++ lib/campaigns_service.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
+require 'rest-client'
+require 'json-diff'
+
 # CampaignsService is a service ...
 module CampaignsService
   class << self
-    def call; end
+    def call(url: nil)
+      return { success: false, error_message: 'No URL' } if url.nil?
+
+      local_campaigns = load_campaigns
+      response = RestClient.get(url, content_type: 'application/json') 
+      remote_campaigns = JSON.parse(response.body)['ads']
+      result = JsonDiff.diff(local_campaigns, remote_campaigns)
+      { success: true, result: result }
+
+    rescue JSON::ParserError
+      { success: false, error_message: 'Not a HASH response' }
+    rescue RestClient::ExceptionWithResponse => e
+      { success: false, error_message: e.message }
+    end
+
+    private
+
+    # Any way to extract campaigns through the Campaign
+    def load_campaigns; end
   end
 end
--- spec/services/campaigns_service_spec.rb
+++ spec/services/campaigns_service_spec.rb
@@ -4,11 +4,41 @@ require 'spec_helper'
 require_relative '../../lib/campaigns_service'
 
 describe 'Campaigns Service' do
-  xcontext 'process URL parameter' do
-    it 'should not send empty url' do; end
-    it 'should process correct URL' do; end
-    it 'should process Bad URLs' do; end
-    it 'should process NOT a HASH response' do; end
+  context 'process URL parameter' do
+    before do
+      allow(CampaignsService).to receive(:load_campaigns).and_return({})
+    end
+
+    it 'should not send empty url' do
+      response = CampaignsService.call
+      expect(response[:success]).to be_falsey
+    end
+
+    it 'should process correct URL' do
+      VCR.use_cassette('200_response') do
+        url = 'https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df'
+        response = CampaignsService.call(url: url)
+        expect(response[:success]).to be_truthy
+      end
+    end
+
+    it 'should process Bad URLs' do
+      VCR.use_cassette('404_response') do
+        url = 'https://httpstat.us/404'
+        response = CampaignsService.call(url: url)
+        expect(response[:success]).to be_falsey
+        expect(response[:error_message]).to eq '404 Not Found'
+      end
+    end
+
+    it 'should process NOT a HASH response' do
+      VCR.use_cassette('NOT_HASH_response') do
+        url = 'https://httpstat.us'
+        response = CampaignsService.call(url: url)
+        expect(response[:success]).to be_falsey
+        expect(response[:error_message]).to eq 'Not a HASH response'
+      end
+    end
   end
 
   xcontext 'detect discrepancies' do
