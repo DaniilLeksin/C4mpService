b8d6b62c4cfcccb29e23468ff5ae11add0809f75
Daniil Leksin <danilaleksin@gmail.com>
2019-01-27 22:12:27 +0300

    Presetup project structure, specs with VCR, test coverage with simplecov

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'rspec'
+gem 'simplecov', require: false, group: :test
+gem 'vcr', '~> 3.0', '>= 3.0.1'
+gem 'webmock', '~> 2.1'
---
+++ spec/services/campaigns_service_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Campaigns Service' do
+  it 'true is true' do
+    expect(true).to be_truthy
+  end
+end
