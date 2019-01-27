80e3e10193907e78aafa18e13e5fc354e317e2b4
Daniil Leksin <danilaleksin@gmail.com>
2019-01-28 00:09:45 +0300

    Add logic. + Formatting

--- lib/campaigns_service.rb
+++ lib/campaigns_service.rb
@@ -4,21 +4,65 @@ require 'rest-client'
 require 'json-diff'
 
 # CampaignsService is a service ...
-module CampaignsService
+class CampaignsService
   class << self
     def call(url: nil)
       return { success: false, error_message: 'No URL' } if url.nil?
 
       local_campaigns = load_campaigns
-      response = RestClient.get(url, content_type: 'application/json') 
+      response = RestClient.get(url, content_type: 'application/json')
       remote_campaigns = JSON.parse(response.body)['ads']
       result = JsonDiff.diff(local_campaigns, remote_campaigns)
-      { success: true, result: result }
-
+      formatted = format_it(result: result, local_campaigns: local_campaigns, remote_campaigns: remote_campaigns)
+      { success: true, result: formatted }
     rescue JSON::ParserError
       { success: false, error_message: 'Not a HASH response' }
     rescue RestClient::ExceptionWithResponse => e
       { success: false, error_message: e.message }
+    end
+
+    def format_it(result:, local_campaigns:, remote_campaigns:)
+      formatted_result = []
+      grouped_result = result.group_by { |diff| diff['path'].split('/').reject!(&:empty?).first }
+
+      grouped_result.each do |k, v|
+        remote_reference = remote_campaigns[k.to_i]
+        discrepancies = []
+        discrepancy_item = {}
+        v.each do |discrepancy|
+          case discrepancy['op']
+          when 'replace'
+            option = discrepancy['path'].split('/').reject!(&:empty?).last
+            discrepancy_item[option] = { 'remote' => discrepancy['value'], 'local' => local_campaigns[k.to_i][option] }
+            discrepancies.push(discrepancy_item)
+          when 'remove'
+            if remote_reference.nil?
+              new_item_index = discrepancy['path'].split('/').reject!(&:empty?).last
+              discrepancies.push('remote' => '', 'local' => local_campaigns[new_item_index.to_i])
+            else
+              new_option = discrepancy['path'].split('/').reject!(&:empty?).last
+              discrepancies.push('remote' => '', 'local' => { new_field: { name: new_option, data: local_campaigns[k.to_i][new_option] } })
+            end
+          when 'add'
+            path = discrepancy['path'].split('/').reject!(&:empty?)
+            if path.one?
+              discrepancies.push('remote' => discrepancy['value'], 'local' => '')
+            else
+              option = discrepancy['path'].split('/').reject!(&:empty?).last
+              discrepancy_item[option] = { 'remote' => discrepancy['value'], 'local' => '' }
+              discrepancies.push(discrepancy_item)
+            end
+          else
+            return { success: false, error_message: 'something strange' }
+          end
+        end
+        formatted_result.push ({
+          'remote_reference' => remote_reference.nil? ? 'missed' : remote_reference['reference'],
+          'discrepancies' => discrepancies
+        })
+      end
+
+      formatted_result
     end
 
     private
--- spec/services/campaigns_service_spec.rb
+++ spec/services/campaigns_service_spec.rb
@@ -7,6 +7,7 @@ describe 'Campaigns Service' do
   context 'process URL parameter' do
     before do
       allow(CampaignsService).to receive(:load_campaigns).and_return({})
+      allow(CampaignsService).to receive(:format_it).and_return({})
     end
 
     it 'should not send empty url' do
@@ -41,13 +42,96 @@ describe 'Campaigns Service' do
     end
   end
 
-  xcontext 'detect discrepancies' do
-    it 'should be OK without discrepancies' do; end
-    it 'should detect discrepancies when field value(s) is/are changed' do; end
-    it 'should detect missed campaign(s)' do; end
-    it 'should detect new field(s) in campaign' do; end
-    it 'should detect new field(s) in existing campaign' do; end
-    it 'should detect removed campaign(s)' do; end
-    it 'should detect removed campaign(s)' do; end
+  context 'detect discrepancies' do
+    before do
+      @campaigns = [
+        { 'reference' => '1', 'status' => 'enabled', 'description' => 'Description for campaign 11' },
+        { 'reference' => '2', 'status' => 'disabled', 'description' => 'Description for campaign 12' },
+        { 'reference' => '3', 'status' => 'enabled', 'description' => 'Description for campaign 13' }
+      ]
+    end
+
+    it 'should be OK without discrepancies' do
+      allow(CampaignsService).to receive(:load_campaigns).and_return(@campaigns)
+      VCR.use_cassette('200_response') do
+        url = 'https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df'
+        response = CampaignsService.call(url: url)
+        expect(response[:result]).to be_empty
+      end
+    end
+
+    it 'should detect discrepancies when field value(s) is/are changed' do
+      @campaigns.first['status'] = 'disabled'
+      @campaigns.first['description'] = 'Description for campaign 00'
+      @campaigns.last['description'] = 'Description for campaign 55'
+
+      allow(CampaignsService).to receive(:load_campaigns).and_return(@campaigns)
+
+      VCR.use_cassette('200_response') do
+        url = 'https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df'
+        response = CampaignsService.call(url: url)
+
+        first_changed = response[:result].first
+        second_changed = response[:result].last
+
+        expect(response[:result].count).to eq(2)
+        expect(first_changed['discrepancies'].count).to eq(1)
+        expect(second_changed['discrepancies'].count).to eq(2)
+      end
+    end
+
+    it 'should detect missed campaign(s)' do
+      @campaigns.push('reference' => '4', 'status' => 'enabled', 'description' => 'Description for campaign 14')
+      @campaigns.push('reference' => '5', 'status' => 'enabled', 'description' => 'Description for campaign 15')
+      allow(CampaignsService).to receive(:load_campaigns).and_return(@campaigns)
+
+      VCR.use_cassette('200_response') do
+        url = 'https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df'
+        response = CampaignsService.call(url: url)
+
+        expect(response[:result].count).to eq(2)
+        expect(response[:result].first['remote_reference']).to eq('missed')
+      end
+    end
+
+    it 'should detect new field(s) in campaign' do
+      @campaigns.first['new_filed_one'] = 'some_data_one'
+      @campaigns.first['new_filed_two'] = 'some_data_two'
+      allow(CampaignsService).to receive(:load_campaigns).and_return(@campaigns)
+
+      VCR.use_cassette('200_response') do
+        url = 'https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df'
+        response = CampaignsService.call(url: url)
+
+        expect(response[:result].count).to eq(1)
+        expect(response[:result].first['remote_reference']).to eq(@campaigns.first['reference'])
+      end
+    end
+
+    it 'should detect removed campaign(s)' do
+      @campaigns.delete_at(2)
+      @campaigns.delete_at(0)
+      allow(CampaignsService).to receive(:load_campaigns).and_return(@campaigns)
+
+      VCR.use_cassette('200_response') do
+        url = 'https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df'
+        response = CampaignsService.call(url: url)
+
+        expect(response[:result].count).to eq(2)
+      end
+    end
+
+    it 'should detect removed campaign(s)' do
+      @campaigns.first.delete('status')
+      @campaigns.last.delete('description')
+      allow(CampaignsService).to receive(:load_campaigns).and_return(@campaigns)
+
+      VCR.use_cassette('200_response') do
+        url = 'https://mockbin.org/bin/fcb30500-7b98-476f-810d-463a0b8fc3df'
+        response = CampaignsService.call(url: url)
+
+        expect(response[:result].count).to eq(2)
+      end
+    end
   end
 end
